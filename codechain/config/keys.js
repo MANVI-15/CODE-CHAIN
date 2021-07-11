fb0fe526e00da4fab657b72a0833e61db4f276f2
MANVI-15 <62294311+MANVI-15@users.noreply.github.com>
2021-07-11 17:50:28 +0530

    Update keys.js

--- codechain/config/keys.js
+++ codechain/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     //MongoURI: 'mongodb+srv://kavisharma59:kavi9130910@cluster0.pb1b6.mongodb.net/my_database?retryWrites=true&w=majority'
-    MongoURI:"mongodb+srv://MANVI:joker15413@cluster0.iu9yg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MongoURI:" "
     
 }
